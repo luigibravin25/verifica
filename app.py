621adbc73cca474a5b0ce62d6f87e1a10b0ae0d2
luigi <luigi.bravin.25@stud.itsaltoadriatico.it>
2026-03-03 10:39:24 +0100

    seconda modifica

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Apertura del file.txt
 with open("file.txt", "r") as f:
-    p = f.readlines()
+    r = f.readlines()
     
+
 #quante righe ci sono nel file
-print(len(p))
+print(f"numero righe: {len(r)}")

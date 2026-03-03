7a2571118fdff13c417a718ee98f8228bb7b6e9a
luigi <luigi.bravin.25@stud.itsaltoadriatico.it>
2026-03-03 10:58:26 +0100

    modifica1

--- app.py
+++ app.py
@@ -4,10 +4,12 @@
 with open("file.txt", "r") as f:
     r = f.read()
 
-    p = r.split()
 
+    l = r.splitlines()
+    print("numero delle linee: ", len(l)) 
+    
+    p = r.split()
     print("conta parole: ",len(p))
 
-#quante righe ci sono nel file
-#print(f"numero righe: {len(r)}")
+    
 
